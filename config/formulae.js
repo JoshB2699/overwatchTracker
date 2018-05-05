e2380dbadea622b7d91eec69805435f0d15813db
Josh Burke <joshb2699@gmail.com>
2018-05-05 07:37:20 +0100

    Added useful comments, updated bootstrap, fixed multi-select properly and added formulae for stats and graphs

---
+++ config/formulae.js
@@ -0,0 +1,183 @@
+var jbestMap = function(games){
+  var bestMap = {mapName: "", winPercent: 0, noOfGames: 0};
+
+  var mapWinPercentArr = getMapData(games);
+
+  for(var i = 0; i < mapWinPercentArr.length; i++){
+    mapWinPercentArr[i].winPercent = mapWinPercentArr[i].wins / mapWinPercentArr[i].noOfGames;
+  }
+
+  for(var i = 0; i < mapWinPercentArr.length; i++){
+    if(mapWinPercentArr[i].winPercent > bestMap.winPercent){
+      bestMap = mapWinPercentArr[i];
+    }
+    if(mapWinPercentArr[i].winPercent === bestMap.winPercent && mapWinPercentArr[i].noOfGames > bestMap.noOfGames){
+      bestMap = mapWinPercentArr[i];
+    }
+  }
+
+  return bestMap;
+}
+
+module.exports.bestMap = jbestMap;
+
+var jbestHero = function(games){
+  var bestHero = {hero: "", winPercent: 0, noOfGames: 0};
+
+  var heroWinPercentArr = getHeroData(games);
+
+  for(var i = 0; i < heroWinPercentArr.length; i++){
+    heroWinPercentArr[i].winPercent = heroWinPercentArr[i].wins / heroWinPercentArr[i].noOfGames;
+  }
+
+  for(var i = 0; i < heroWinPercentArr.length; i++){
+    if(heroWinPercentArr[i].winPercent > bestHero.winPercent){
+      bestHero = heroWinPercentArr[i];
+    }
+    if(heroWinPercentArr[i].winPercent === bestHero.winPercent && heroWinPercentArr[i].noOfGames > bestHero.noOfGames){
+      bestHero = heroWinPercentArr[i];
+    }
+  }
+
+  return bestHero;
+}
+
+module.exports.bestHero = jbestHero;
+
+var jheroPlaytime = function(games){
+  var heroPlaytime = [];
+
+  var heroDataArr = getHeroData(games);
+
+  heroDataArr.forEach(function(heroData){
+    heroPlaytime.push([heroData.hero, heroData.noOfGames]);
+  });
+
+
+
+  heroPlaytime.sort(function(a,b){
+    if(a[1]<b[1]){
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  heroPlaytime.unshift(["Hero", "Playtime"]);
+
+  return heroPlaytime;
+}
+
+module.exports.heroPlaytime = jheroPlaytime;
+
+var jgameModePlaytime = function(games, maps){
+
+  var assault = 0;
+  var hybrid = 0;
+  var control = 0;
+  var escort = 0;
+
+  var mapDataArr = getMapData(games);
+
+  mapDataArr.forEach(function(mapData){
+    switch(getMapType(mapData.mapName, maps)){
+      case "assault":
+        assault++;
+      case "hybrid":
+        hybrid++;
+      case "control":
+        control++;
+      case "escort":
+        escort++;
+    }
+  });
+
+  gameModePlaytime = [["Assault", assault], ["Hybrid", hybrid], ["Control", control], ["Escort", escort]]
+
+
+  gameModePlaytime.sort(function(a,b){
+    if(a[1]<b[1]){
+      return 1;
+    } else {
+      return -1;
+    }
+  });
+
+  gameModePlaytime.unshift(["Game Mode", "Playtime"]);
+
+  return gameModePlaytime;
+}
+
+module.exports.gameModePlaytime = jgameModePlaytime;
+
+
+function getMapData(games){
+
+  var gameData = [];
+
+  games.forEach(function(game) {
+
+    var x = gameData.find(e => e.mapName === game.mapName);
+
+    var winLose;
+    if(game.winLose){
+      winLose = 1;
+    } else {
+      winLose = 0;
+    }
+
+    if(typeof(x) == "undefined"){
+        gameData.push({ mapName: game.mapName, wins: winLose, noOfGames: 1 });
+      } else {
+        let index = gameData.indexOf(x);
+        gameData.fill(x.wins += winLose, index, index++);
+        gameData.fill(x.noOfGames++, index, index++);
+      }
+
+  });
+
+  return gameData;
+}
+
+function getHeroData(games){
+
+  var gameData = [];
+
+  games.forEach(function(game) {
+
+    for(var i = 0; i < game.heroes.length; i++){
+
+      var x = gameData.find(e => e.hero === game.heroes[i]);
+
+      var winLose;
+      if(game.winLose){
+        winLose = 1;
+      } else {
+        winLose = 0;
+      }
+
+      if(typeof(x) == "undefined"){
+        gameData.push({ hero: game.heroes[i], wins: winLose, noOfGames: 1 });
+      } else {
+        let index = gameData.indexOf(x);
+        gameData.fill(x.wins += winLose, index, index++);
+        gameData.fill(x.noOfGames++, index, index++);
+      }
+
+    }
+  });
+
+  return gameData;
+}
+
+function getMapType(name, maps){
+  var x = maps.find(e => e.name === name);
+
+  if(typeof(x) == "undefined"){
+    throw new Error('The map could not be found in the list of maps.\nMap searched for: ' + name);
+  } else {
+    return x.mapType;
+  }
+
+
+}
--- config/routes.js
+++ config/routes.js
@@ -3,15 +3,22 @@ const Map = require('../models/map.js');
 const Game = require('../models/game.js');
 const Hero = require('../models/hero.js');
 
+const formulae = require('../config/formulae.js');
+
 module.exports = function(app, mongoose, uuid){
 
+  //On GET / render index.ejs to the DOM.
   app.get('/', function(req, res){
     res.render('index');
   });
 
+  //On GET /input
   app.get('/input', function(req, res){
+
+    //Search the mongoose database for all maps.
     Map.find({}, function(err, maps){
 
+      //Sort the maps first by map type, then by name alphabetically.
        maps.sort(function(a,b){
           if (a.mapType === b.mapType){
             if(a.name>b.name){
@@ -26,8 +33,10 @@ module.exports = function(app, mongoose, uuid){
           }
        });
 
+       //Search the databse for all heroes.
        Hero.find({}, function(err,heroes){
 
+         //Sort them alphabetically by name.
          heroes.sort(function(a,b){
           if(a.name>b.name){
             return 1;
@@ -36,6 +45,7 @@ module.exports = function(app, mongoose, uuid){
           }
          });
 
+         //Render input.ejs and send the maps and heroes array.
         res.render('input', {
           maps: maps,
           heroes: heroes
@@ -44,47 +54,102 @@ module.exports = function(app, mongoose, uuid){
     });
   });
 
+  //On GET /stats
   app.get('/stats', function(req, res){
-    res.render('stats');
+    //Create an empty object that we will store the statistics in.
+    var stats = {};
+
+    Hero.find({}, function(err,heroes){
+      Map.find({}, function(err, maps){
+        Game.find({}, function(err, games){
+
+          stats.bestMap = formulae.bestMap(games);
+          stats.bestHero = formulae.bestHero(games);
+
+          //Render input.ejs and send the maps and heroes array.
+          res.render('stats', {
+            stats: stats
+          });
+        });
+      });
+    });
+
+
+
   });
 
+  //On GET /graphs
   app.get('/graphs', function(req, res){
-    res.render('graphs');
+    //Create an empty object that we will store the statistics in.
+    var stats = {};
+
+    Hero.find({}, function(err,heroes){
+      Map.find({}, function(err, maps){
+        Game.find({}, function(err, games){
+
+          stats.heroPlaytime = formulae.heroPlaytime(games);
+          stats.gameModePlaytime = formulae.gameModePlaytime(games, maps);
+
+          console.log(stats.gameModePlaytime)
+
+          //Render input.ejs and send the maps and heroes array.
+          res.render('graphs', {
+            stats: stats
+          });
+        });
+      });
+    });
+
   });
 
+  //On GET /admin
   app.get('/admin', function(req, res){
+    //Render admin.ejs to the DOM.
     res.render('admin');
   });
 
+  //On POST /admin get the type of post.
   app.post('/admin/:type', function(req, res){
+    //If the type is map.
     if(req.params.type == "map"){
 
+      //Create a new map object with parameters of mapName and mapType from the page.
       var map = new Map({
         name: req.body.mapName,
         mapType: req.body.mapType
       });
 
+      //Save the map object to the database.
       map.save(function(err) {
           if (err){
+            //If an error occurred then throw it.
             throw err;
           } else {
+            //Otherwise log a message in the console saying that the map was added to the database.
             console.log("Map successfully added: " + req.body.mapName + " Type: " + req.body.mapType);
+            //Then redirect the browser to /admin.
             res.redirect('/admin');
           }
       });
 
+      //Otherwise if the type is hero.
     } else if (req.params.type == "hero"){
 
+      //Create a new hero object with parameters of heroName and heroType from the page.
       var hero = new Hero({
         name: req.body.heroName,
         type: req.body.heroType
       });
 
+      //Save the hero obejct to the database.
       hero.save(function(err) {
             if (err){
+              //If an error occurred then throw it.
               throw err;
             } else {
+              //Otherwise log a message in the console saying that the hero was added to the database.
               console.log("Map successfully added: " + req.body.heroName + " Type: " + req.body.heroType);
+              //Then redirect the browser to /admin.
               res.redirect('/admin');
             }
         });
@@ -92,9 +157,9 @@ module.exports = function(app, mongoose, uuid){
 
   });
 
-
+  //On POST /input
   app.post('/input', function(req, res){
-
+    //Create a new game object with parameters of a generated uuid; and map, winLose and heroes from the page.
     var game = new Game({
       uuid: uuid(),
       mapName: req.body.map,
@@ -102,10 +167,13 @@ module.exports = function(app, mongoose, uuid){
       heroes: req.body.heroes
     });
 
+    //Save the game object to the database.
     game.save(function(err) {
           if (err){
+            //If an error occurred then throw it.
             throw err;
           } else {
+            //Otherwise redirect the user to /input.
             res.redirect('/input');
           }
       });
